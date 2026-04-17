9e82eb244a7cbb77c9891d5864356916dbc6c49e
Garikoitz Lerma-Usabiaga <garikoitz@gmail.com>
2026-04-17 21:01:54 +0200

    Tiger and Ruiqing make do_screen and eyetracker work for the Lenovo

--- functions/do_screen.m
+++ functions/do_screen.m
@@ -11,22 +11,42 @@ function [w, center] = doScreen
     screen_num = 0;  % Use 0 instead of max(Screen('Screens'))
 
     % Get full screen dimensions
-    %screen_rect = Screen('Rect', screen_num);
-
-    % Define rect for VGA-1 (which starts at x = 1920)
-    % second_screen_rect = [1920, 0, 2944, 768];  % 1080 VGA screen size is 1024x768
-    % second_screen_rect = [1920, 0, 2944, 600];
-    % dev_display_x = 2560;
-    % dev_display_y = 1440;
+    screen_rect = Screen('Rect', screen_num);
+    %% Bellow is for running on MRI lab
+        % Special note
+        % in the MRI lab, when we connect the DP (the DELL screen) and VGA (the PROJECTOR screen), 
+        % the VGA is being recognized as DP-2, so we have to treat the 2
+        % screens as one
+        % resolution for dell: 1920 x 1080, 
+        % resolution for Projector is 1920 x 1080 (it default as 1024 x 764, we change the scale )
+        % And the resolution for x-axis is 2944 (which is the 1920 of dell merged with 1024 VGA)
+    % in the previous test code is [1920, 0, 2944, 768]
+    % in the MRI lab DELL 23 inch  is [1920, 0, 3840, 1080]
+    second_screen_rect = [1920, 0, 3840, 1080];  % VGA screen size is 1024x768 
+    
+    %% Below is for testing on one screen
+    % % X: in the previous test code is 2560 
+    % % X: in the MRI lab DELL 23 inch is 1920
+    % dev_display_x = 1920; 
+    % % Y: in the previous test code is 1440 
+    % % Y: in the MRI lab DELL 23 inch is 1080
+    % dev_display_y = 1080;
+    % 
+    % 
+    % % this is for the DISPLAY in one screen
     % stim_display_x = 1024;
-    % stim_display_y = 768;
+    % stim_display_y = 1080;
     % second_screen_rect = [dev_display_x - stim_display_x, ...
     %                       0, ...
     %                       dev_display_x, ...
     %                       stim_display_y]; % current resolution is 2560 x 1440
-    
+    %                                        % lab dell is 1920 x 1080 
+    % 
     
     % Open the window on the selected screen using its full rect
+    % note: command is OpenWindow, screen_num is 0, 128 is grey scale,
+    % rect is where you want to display the stuff, in this case we want the
+    % whole second screen so we input the [1920, 0, 3840, 1080 ]
     [w, rect] = Screen('OpenWindow', screen_num, 128, second_screen_rect);
 
     % Get center coordinates of the screen
